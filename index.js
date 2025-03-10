2fcb68fa0186dd29d462a743fae3826c74cf1048
MillaMirjami <milla.sukki@gmail.com>
2025-03-10 10:40:40 +0200

    Cors middleware poistettu käytöstä, koska viten avulla frontin ja bäkkärin polut on konfiguroitu samaan originiin

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 const express = require('express')
 const app = express()
-const cors = require('cors')
 
 let notes = [
     {
@@ -19,7 +18,6 @@ let notes = [
         important: true
     }
 ]
-app.use(cors())
 app.use(express.json())
 app.use(express.static('dist'))
 // Otetaan käyttöön json expressille, jotta voidaan muuttaa json-merkkijono js-olioksi ja toisinpäin.
