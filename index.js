9e53e96025f999c29bc50b15a2c5eb92e4737e08
MillaMirjami <milla.sukki@gmail.com>
2025-03-12 14:25:15 +0200

    Renderiin lisätty env ja index.js:ssä muutettu osa pyynnönkäsittelijöistä. dotenv asennettu myös

--- index.js
+++ index.js
@@ -1,24 +1,8 @@
+require('dotenv').config()
 const express = require('express')
 const app = express()
 const Note = require('./Models/note')
 
-// let notes = [
-//     {
-//         id: "1",
-//         content: "HTML is easy",
-//         important: true
-//     },
-//     {
-//         id: "2",
-//         content: "Browser can execute only JavaScript",
-//         important: false
-//     },
-//     {
-//         id: "3",
-//         content: "GET and POST are the most important methods of HTTP protocol",
-//         important: true
-//     }
-// ]
 app.use(express.json())
 app.use(express.static('dist'))
 // Otetaan käyttöön json expressille, jotta voidaan muuttaa json-merkkijono js-olioksi ja toisinpäin.
@@ -39,18 +23,10 @@ app.get('/api/notes', (request, response) => {
     Note.find({}).then(notes => {
         response.json(notes)
     })
-    // response.json(notes)
 })
 
 app.get('/api/notes/:id', (request, response) => {
-    const id = request.params.id
-    const note = notes.find(note => note.id === id)
-
-    if(note){
-        response.json(note)
-    } else {
-        response.status(404).end()
-    }
+    Note.findById(request.params.id).then(note => response.json(note))
 })
 
 app.delete('/api/notes/:id', (request, response) => {
@@ -86,19 +62,18 @@ app.post('/api/notes', (request, response) => {
         return response.status(400).json({error: "content missing"})
     }
     
-    const note = {
-        id: generateId(),
+    const note = new Note({
         content: body.content,
         important: body.important || false
-    }
+    }) 
 
     requestLogger(request)
-    notes = notes.concat(note)
-    response.json(note) // tämä muuttaa js-olion takaisin json-merkkijonoksi ja lähettää sen näin takaisin asiakkaalle.
+    note.save().then(savedNote => response.json(savedNote))
+     // .json muuttaa js-olion takaisin json-merkkijonoksi ja lähettää sen näin takaisin asiakkaalle.
 })
 
 
-const PORT = process.env.PORT || 3001
+const PORT = process.env.PORT
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)
 })
