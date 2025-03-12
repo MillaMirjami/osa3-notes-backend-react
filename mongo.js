1c5f84ee05b5c2da870c043baf96930bc81fc4eb
MillaMirjami <milla.sukki@gmail.com>
2025-03-12 11:58:52 +0200

    Mondodb luotu ja .js filu luotu, jossa luodaan yhteys, skeema, malli, uusia olioita kantaan ja suljetaan yhteys

--- mongo.js
+++ mongo.js
@@ -7,7 +7,7 @@ if (process.argv.length < 3) {
 
 const password = process.argv[2]
 
-const url = `mongodb+srv://fullstack:${password}@cluster0.a5qfl.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`
+const url = `mongodb+srv://fullstack:${password}@cluster0.5i94f.mongodb.net/NoteApp?retryWrites=true&w=majority&appName=Cluster0`
 
 mongoose.set('strictQuery', false)
 mongoose.connect(url)
@@ -19,12 +19,17 @@ const noteSchema = new mongoose.Schema({
 
 const Note = mongoose.model('Note', noteSchema)
 
-const note = new Note({
-  content: 'HTML is easy',
-  important: true,
-})
-
-note.save().then(result => {
-  console.log('note saved!')
+Note.find({important: false}).then(result => {
+  result.forEach(note => console.log(note))
   mongoose.connection.close()
 })
+
+// const note = new Note({
+//   content: 'You need to know everything about Programming',
+//   important: false,
+// })
+
+// note.save().then(result => {
+//   console.log('note saved!', result)
+//   mongoose.connection.close()
+// })
